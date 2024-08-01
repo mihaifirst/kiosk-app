3dda8a254ac3dfa20c11b04f3f201486f2a3d829
mihaifirst <varzaru.alexmihai@gmail.com>
2024-08-01 13:10:47 +0300

    axios added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [bundleSettings, setBundleSetting] = useState({});
+
+  function getBundleSettings() {
+    axios
+      .get("http://localhost:3333/api/getBundleSettings")
+      .then(({ data }) => {
+        console.log(data);
+        setBundleSetting(data);
+        console.log(bundleSettings);
+      })
+      .catch((error) => console.log(error));
+  }
 
   return (
     <>
@@ -18,9 +30,6 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
@@ -29,7 +38,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
